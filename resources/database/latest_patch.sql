312addd8c820aeea2e46dfeb9ec367319848ce26
sergey <sergey@hpnotebook>
2014-02-20 07:22:57 +0400

    +

--- resources/database/latest_patch.sql
+++ resources/database/latest_patch.sql
@@ -1,9 +1,9 @@
 #14.02.14
-ALTER TABLE `ekoshka`.`store_order`
+ALTER TABLE `store_order`
 ADD COLUMN `phone` VARCHAR(20) NULL DEFAULT NULL
 AFTER `city`;
 
-ALTER TABLE `ekoshka`.`privacy_address_record`
+ALTER TABLE `privacy_address_record`
 CHANGE COLUMN `postcode` `postcode` VARCHAR(10) NULL DEFAULT NULL,
 CHANGE COLUMN `region` `region` VARCHAR(145) NULL DEFAULT NULL,
 CHANGE COLUMN `city` `city` VARCHAR(145) NULL DEFAULT NULL,
@@ -11,4 +11,4 @@ ADD COLUMN `phone` VARCHAR(20) NULL DEFAULT NULL
 AFTER `lastName`;
 
 
-INSERT INTO `billiongoods`.`system_version` (`version`) VALUES ('140214');
+INSERT INTO `system_version` (`version`) VALUES ('140214');
